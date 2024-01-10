666a5a91563df2d4e4555249dc6ccae28c5c6355
Vilasagaram Saicharan <123568962+sidsai2804@users.noreply.github.com>
2024-01-10 19:12:43 +0530

    OTP verification.py

--- OTP.py
+++ OTP.py
@@ -5,22 +5,17 @@ def generate_otp():
     return str(random.randint(100000, 999999))
 
 otp = generate_otp()
-print(f"Generated OTP: {otp}")
 
+email = "unknowngamer28o4@gmail.com"
+password = "leagntwoebwjwcnz"
+recever_mail = "sidsai28@gmail.com"
 
-#smtp --> simple Mail Transfer Protocol
-
-
-email = "***********@gmail.com"
-password = "*************"
-recever_mail = "*********@gmail.com"
-
-message = f'''FROM : {email}
+message = f'''FROM: {email}
 TO: {recever_mail}
 CC: {recever_mail}
-SUBJECT :  Another mail \n
-This is your one time password {otp}'''
-# POP, IMAP , SMTP
+SUBJECT: Another mail \n
+This is your one-time password {otp}'''
+
 server = smtplib.SMTP("smtp.gmail.com", 587)
 
 server.starttls()
@@ -28,3 +23,10 @@ server.login(email, password)
 server.sendmail(email, recever_mail, message)
 print("Email sent")
 server.quit()
+
+user_entered_otp = input("Enter the OTP received in your email: ")
+
+if user_entered_otp == otp:
+    print("OTP verification successful! Access granted.")
+else:
+    print("Incorrect OTP. Access denied.")
